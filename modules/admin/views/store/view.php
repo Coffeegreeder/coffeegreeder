d2fee9eb26d78fa30149195630a4262fcdd92116
Coffeegreeder <Coffeegreder@gmail.com>
2021-05-18 16:44:42 +0300

    fileUpload+admin fix

--- modules/admin/views/store/view.php
+++ modules/admin/views/store/view.php
@@ -30,15 +30,14 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute'=> 'img_before',
                 'value' => function($model){
-
-                    '/'return Html::img($model->img_before, ['width' => 150]);
+                    return echo '/'.Html::img($model->img_before, ['width' => 150]);
                 },
                 'format' => 'html'
             ],
             [
                 'attribute'=> 'img_after',
                 'value' => function($model){
-                    '/'return Html::img($model->img_after, ['width' => 150]);
+                    return echo '/'.Html::img($model->img_after, ['width' => 150]);
                 },
                 'format' => 'html'
             ],
